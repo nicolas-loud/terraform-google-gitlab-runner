ed799fd71e73b4e6b41abcd235787a950ee963db
mamadouberthe <mamadou.berthe-ext@infinitif.com>
2025-04-09 10:39:16 +0200

    pass boot image in param

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "google_compute_instance" "ci_runner" {
 
   boot_disk {
     initialize_params {
-      image = "centos-cloud/centos-7"
+      image = var.ci_runner_boot_image
       size  = var.ci_runner_disk_size
       type  = "pd-standard"
     }
